f8155c4fde8b4d3740e6dbeb0a7c66eaacb2c8f0
AlexandreCordaJunior <alexandre.corda@fatec.sp.gov.br>
2020-01-03 11:13:06 -0300

    descompactado

---
+++ DesafioHomeOfficeG4/webapp/controller/Questao03.controller.js
@@ -0,0 +1,55 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.desafioHomeOffice.controller.Questao03", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5e0e3155cf5bb2780c3d3c58";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		avatarInitialsFormatter: function(sTextValue) {
+			return typeof sTextValue === 'string' ? sTextValue.substr(0, 2) : undefined;
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Questao03").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		}
+	});
+}, /* bExport= */ true);
